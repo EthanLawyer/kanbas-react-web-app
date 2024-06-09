6032aa13738a51dff20c06b5e160746d110d50a2
EthanLawyer <zhu.yixiao@outlook.com>
2024-06-09 15:12:29 +0800

    Selecting content with class selectors

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -7,12 +7,12 @@ export default function Lab2() {
         <h3>Styling with the STYLE attribute</h3>
         {/* <p style={{ backgroundColor: "blue",
                     color: "white" }}> */}
-        {/* <p>
+        <p>
           Style attribute allows configuring look and feel
           right on the element. Although it's very convenient
           it is considered bad practice and you should avoid
           using the style attribute
-        </p> */}
+        </p>
         <div id="wd-css-id-selectors">
           <h3>ID selectors</h3>
           <p id="wd-id-selector-1">
@@ -24,7 +24,17 @@ export default function Lab2() {
             feel
           </p>
         </div>
+        <div id="wd-css-class-selectors">
+          <h3>Class selectors</h3>
+          <p className="wd-class-selector">
+            Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+          </p>
+          <h4 className="wd-class-selector">
+            This heading has same style as paragraph above
+          </h4>
+        </div>
       </div>
+      
     );
   }
   
