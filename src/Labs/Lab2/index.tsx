0e2218d23cd6d08ffe49eac9b3b3c7957664b4f0
EthanLawyer <zhu.yixiao@outlook.com>
2024-06-09 14:49:54 +0800

    Selecting content with ID selectors

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -7,12 +7,23 @@ export default function Lab2() {
         <h3>Styling with the STYLE attribute</h3>
         {/* <p style={{ backgroundColor: "blue",
                     color: "white" }}> */}
-        <p>
+        {/* <p>
           Style attribute allows configuring look and feel
           right on the element. Although it's very convenient
           it is considered bad practice and you should avoid
           using the style attribute
-        </p>
+        </p> */}
+        <div id="wd-css-id-selectors">
+          <h3>ID selectors</h3>
+          <p id="wd-id-selector-1">
+            Instead of changing the look and feel of all the 
+            elements of the same name, e.g., P, we can refer to a specific element by its ID
+          </p>
+          <p id="wd-id-selector-2">
+            Here's another paragraph using a different ID and a different look and
+            feel
+          </p>
+        </div>
       </div>
     );
   }
